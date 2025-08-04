834b191bee69bea4c979b71e7e9c7a5d93ba9424
Ooozari <uzairasiff1227@gmail.com>
2025-08-04 22:38:52 +0500

    fixed the tabs issue

--- src/components/ui/userinfotabs.jsx
+++ src/components/ui/userinfotabs.jsx
@@ -33,7 +33,7 @@ export function TabsTrigger({ className, ...props }) {
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
      className={cn(
-  "inline-flex h-full items-center rounded-[1480.1px] justify-center px-4 font-[800] font-urbanist transition " +
+  "inline-flex h-full items-center rounded-[1480.1px] justify-center px-2 md:px-4 font-[800] font-urbanist transition " +
   "data-[state=active]:bg-[#005F72] data-[state=active]:text-White text-[#3F3C36]" +
   "hover:bg-[#1E365C1A] hover:text-[#005F72] " +
   "disabled:pointer-events-none cursor-pointer disabled:opacity-50 ",
