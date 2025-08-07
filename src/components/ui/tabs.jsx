cbd460eb0d21ef1ff3f6b6a55a218fc21e56bce5
Ooozari <uzairasiff1227@gmail.com>
2025-08-07 12:35:22 +0500

    Fix tab spacing to align with heading

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -40,7 +40,7 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:text-Secondary data-[state=active]:border-l-3 data-[state=active]:border-Secondary data-[state=active]:font-[800] dark:data-[state=active]:text-foreground data-[state=active]:ml-3 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-[#292929] cursor-pointer dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-start justify-start font-urbanist px-3 text-lg font-[600] whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1  disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "data-[state=active]:text-Secondary data-[state=active]:border-l-3 data-[state=active]:border-Secondary data-[state=active]:font-[800] dark:data-[state=active]:text-foreground data-[state=active]:pl-3 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-[#292929] cursor-pointer dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-start justify-start font-urbanist text-lg font-[600] whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1  disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props} />
